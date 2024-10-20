3dc7fdf5fb64f31d396caf717b56ef5cbf05cd5e
cage433 <cage433@gmail.com>
2024-10-21 00:21:56 +0100

    Nicely split up

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
-vim.g.mapleader = " "
 
 local options = { noremap = true }
 vim.keymap.set("i", "jk", "<Esc>", options)
@@ -33,31 +27,4 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup("plugins")
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-local configs = require("nvim-treesitter.configs")
-configs.setup({
-  ensure_installed={"lua"}
-})
-
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle reveal left<CR>')
-require("neo-tree").setup({
-        event_handlers = {
-
-          {
-            event = "file_opened",
-            handler = function(file_path)
-              require("neo-tree.command").execute({ action = "close" })
-            end
-          },
-
-        }
-      })
-
-
-require("catppuccin").setup({flavour="mocha"})
-vim.cmd.colorscheme "catppuccin"
-
+require("vim-options")
