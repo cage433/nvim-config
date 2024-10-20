1dd7931733db06473c5e9c44d1ee7115640d85c5
cage433 <cage433@gmail.com>
2024-10-20 23:54:22 +0100

    working

---
+++ init.lua
@@ -0,0 +1,81 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.g.mapleader = " "
+
+local options = { noremap = true }
+vim.keymap.set("i", "jk", "<Esc>", options)
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+local opts = {}
+local plugins = {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", 
+        "MunifTanjim/nui.nvim",
+      }
+    },
+}
+require("lazy").setup(plugins, opts)
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local configs = require("nvim-treesitter.configs")
+configs.setup({
+  ensure_installed={"lua"}
+})
+
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle reveal left<CR>')
+require("neo-tree").setup({
+        event_handlers = {
+
+          {
+            event = "file_opened",
+            handler = function(file_path)
+              require("neo-tree.command").execute({ action = "close" })
+            end
+          },
+
+        }
+      })
+
+
+require("catppuccin").setup({flavour="mocha"})
+vim.cmd.colorscheme "catppuccin"
+
